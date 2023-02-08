fcf7016e129bf6058c5a5d1d1d6c7b3b7559c9fb
Александр Мельников <meln.alex97@gmail.com>
2023-02-09 00:09:55 +0700

    Fix after testing
    Исправление ошибок после тестирования игры

--- Assets/Runner/Scripts/View/PlayerView.cs
+++ Assets/Runner/Scripts/View/PlayerView.cs
@@ -1,9 +1,46 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace Runner.Scripts.View
 {
-    internal class PlayerView: MonoBehaviour
+    [RequireComponent(typeof(SpriteRenderer))]
+    internal class PlayerView : MonoBehaviour
     {
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+        [SerializeField] private Sprite[] _runSprites;
+        [Range(0 , 1)]
+        [SerializeField] private float _animationDelay = 0.1f;
 
+        private bool _playAnimation;
+        private int _currentAnim;
+
+        private void Start()
+        {
+            _currentAnim = 0;
+            _playAnimation = true;
+            StartCoroutine(PlayAnimation());
+        }
+
+        IEnumerator PlayAnimation() 
+        {
+            while (_playAnimation) 
+            {
+                _currentAnim = (_currentAnim + 1) % _runSprites.Length;
+                _spriteRenderer.sprite = _runSprites[_currentAnim];
+
+                yield return new WaitForSeconds(_animationDelay);
+            }           
+        }
+
+        private void OnValidate()
+        {
+            _spriteRenderer ??= GetComponent<SpriteRenderer>();
+        }
+
+        private void OnDestroy()
+        {
+            _playAnimation = false;
+            StopAllCoroutines();
+        }
     }
 }
