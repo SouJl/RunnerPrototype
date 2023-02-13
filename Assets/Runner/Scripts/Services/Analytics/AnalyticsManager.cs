9857f5cbae29f4e864a6a9b11540eb5ea1bb66eb
SouJl <meln.alex97@gmail.com>
2023-02-13 11:02:58 +0700

    Advanced settings in AnalyticsManager

--- Assets/Runner/Scripts/Services/Analytics/AnalyticsManager.cs
+++ Assets/Runner/Scripts/Services/Analytics/AnalyticsManager.cs
@@ -7,13 +7,27 @@ namespace Services.Analytics
 {
     internal interface IAnalyticsManager 
     {
+        bool SendMainMenuOpenEnable { get; }
+        bool SendGameStartedEnable { get; }
+
         void SendMainMenuOpen();
         void SendGameStarted(string gameInput);
     }
 
     internal class AnalyticsManager:MonoBehaviour, IAnalyticsManager
     {
+        [Header("Analytics system")]
         [SerializeField] private bool _useUnityAnalytics;
+        
+        [Space(10)]
+        [Header("Using Analytics Events")]
+        [SerializeField] private bool _sendMainMenuOpenEnable = true;
+        [SerializeField] private bool _sendGameStartedEnabl = true;
+
+
+        public bool SendMainMenuOpenEnable => _sendMainMenuOpenEnable;
+        public bool SendGameStartedEnable => _sendGameStartedEnabl;
+
 
         private List<IAnalyticsService> _services;
 
