ac80c1dd6d866a6ca01214e204a03d4391e95ae3
SouJl <meln.alex97@gmail.com>
2023-02-22 11:09:45 +0700

    Add generics filtration to BaseContext

--- Assets/Runner/Scripts/BaseContext.cs
+++ Assets/Runner/Scripts/BaseContext.cs
@@ -6,7 +6,9 @@ namespace Runner
     internal interface IContext : IDisposable 
     { }
 
-    internal abstract class BaseContext<TConfig, TRepository, TView> : LifeCycleObject, IContext
+    internal abstract class BaseContext<TConfig, TRepository, TView> : LifeCycleObject, IContext 
+        where TRepository : IRepository
+        where TView : MonoBehaviour
     {
         protected abstract TConfig[] LoadConfigs();
 
