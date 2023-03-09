8754e41b878a19fd790668aeb09536de472a3285
Александр Мельников <meln.alex97@gmail.com>
2023-03-09 23:29:30 +0700

    Add audio buttons logic

---
+++ Assets/Runner/_Root/Scripts/Tool/Audio/AudioButtonComponent.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Runner.Tool.Audio
+{
+    [RequireComponent(typeof(Button))]
+    [RequireComponent(typeof(AudioSource))]
+    internal class AudioButtonComponent : MonoBehaviour
+    {
+        [Header("Components")]
+        [SerializeField] protected Button _button;
+        [SerializeField] protected AudioSource _audioSource;
+
+        private void OnValidate() => InitComponents();
+        private void Awake() => InitComponents();
+
+        private void OnDestroy() => _button.onClick.RemoveAllListeners();
+
+        private void InitComponents()
+        {
+            _button ??= GetComponent<Button>();
+            _audioSource ??= GetComponent<AudioSource>();
+
+            _button.onClick.AddListener(OnButtonClick);
+        }
+
+        private void OnButtonClick() => ActivateSound();
+        private void ActivateSound() => _audioSource.Play();
+
+    }
+}
