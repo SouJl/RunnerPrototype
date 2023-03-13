371e5a677130e9f781efca3e47fc40f2dd5be6f7
Александр Мельников <meln.alex97@gmail.com>
2023-03-13 21:32:46 +0700

    Implement load assets with asset browser

--- Assets/Runner/_Root/Scripts/Tool/Bundles/AssetBundleViewBase.cs
+++ Assets/Runner/_Root/Scripts/Tool/Bundles/AssetBundleViewBase.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -6,20 +7,45 @@ namespace Runner.Scripts.Tool.Bundles
 {
     internal class AssetBundleViewBase : MonoBehaviour
     {
-        private const string UrlAssetBundleSprites = "google-drive_url";
-        private const string UrlAssetBundleAudio = "google-drive_url";
+        private const string DownloadSourcePath = "https://drive.google.com/uc?export=download&id=";
 
+        [Header("Id Keys Settings")]
+        [SerializeField] private string singleBundleSpritesId;
+        [SerializeField] private string multiplyBundleSpritesId;
+        [SerializeField] private string audioBundleSpritesId;
+
+        [Header("Sprites")]
         [SerializeField] private DataSpriteBundle[] _dataSpriteBundles;
+        [SerializeField] private DataMultiplySpriteBundle[] _dataMultiplySpriteBundle;
+
+        [Space(10)]
+        [Header("Audio")]
         [SerializeField] private DataAudioBundle[] _dataAudioBundles;
 
 
         private AssetBundle _spritesAssetBundle;
+        private AssetBundle _multiplySpritesAssetBundle;
         private AssetBundle _audioAssetBundle;
+
+
+
+        private string _urlAssetBundleSprites;
+        private string _urlAssetBundleMultiplySprites;
+        private string _urlAssetBundleAudio;
+
+
+        private void Awake()
+        {
+            _urlAssetBundleSprites = DownloadSourcePath + singleBundleSpritesId;
+            _urlAssetBundleMultiplySprites = DownloadSourcePath + multiplyBundleSpritesId;
+            _urlAssetBundleAudio = DownloadSourcePath + audioBundleSpritesId;
+        }
 
 
         protected IEnumerator DownloadAndSetAssetBundles()
         {
             yield return GetSpritesAssetBundle();
+            yield return GetMultiplySpiteAssetBundle();
             yield return GetAudioAssetBundle();
 
             if (_spritesAssetBundle != null)
@@ -27,15 +53,22 @@ namespace Runner.Scripts.Tool.Bundles
             else
                 Debug.LogError($"AssetBundle {nameof(_spritesAssetBundle)} failed to load");
 
+            if (_multiplySpritesAssetBundle != null)
+                SetMultiplySpriteAssets(_multiplySpritesAssetBundle);
+            else
+                Debug.LogError($"AssetBundle {nameof(_multiplySpritesAssetBundle)} failed to load");
+
             if (_audioAssetBundle != null)
                 SetAudioAssets(_audioAssetBundle);
             else
                 Debug.LogError($"AssetBundle {nameof(_audioAssetBundle)} failed to load");
         }
 
+
+
         private IEnumerator GetSpritesAssetBundle()
         {
-            UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(UrlAssetBundleSprites);
+            UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(_urlAssetBundleSprites);
 
             yield return request.SendWebRequest();
 
@@ -45,9 +78,22 @@ namespace Runner.Scripts.Tool.Bundles
             StateRequest(request, out _spritesAssetBundle);
         }
 
+
+        private IEnumerator GetMultiplySpiteAssetBundle()
+        {
+            UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(_urlAssetBundleMultiplySprites);
+
+            yield return request.SendWebRequest();
+
+            while (!request.isDone)
+                yield return null;
+
+            StateRequest(request, out _multiplySpritesAssetBundle);
+        }
+
         private IEnumerator GetAudioAssetBundle()
         {
-            UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(UrlAssetBundleAudio);
+            UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(_urlAssetBundleAudio);
 
             yield return request.SendWebRequest();
 
@@ -75,6 +121,18 @@ namespace Runner.Scripts.Tool.Bundles
         {
             foreach (DataSpriteBundle data in _dataSpriteBundles)
                 data.Image.sprite = assetBundle.LoadAsset<Sprite>(data.NameAssetBundle);
+        }
+
+        private void SetMultiplySpriteAssets(AssetBundle assetBundle)
+        {
+            foreach(DataMultiplySpriteBundle dataMultySprite in _dataMultiplySpriteBundle)
+            {
+                var assetSprites = assetBundle.LoadAssetWithSubAssets<Sprite>(dataMultySprite.NameRootSpiteAsset);
+                foreach (DataSpriteBundle data in dataMultySprite.DataSpriteBundles) 
+                {
+                    data.Image.sprite = assetSprites.Where(s => s.name == data.NameAssetBundle).FirstOrDefault();
+                }
+            }
         }
 
         private void SetAudioAssets(AssetBundle assetBundle)
---
+++ Assets/Runner/_Root/Scripts/Tool/Bundles/DataMultiplySpriteBundle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Runner.Scripts.Tool.Bundles
+{
+    [Serializable]
+    internal class DataMultiplySpriteBundle
+    {
+        [field: SerializeField] public string NameRootSpiteAsset { get; private set; }
+        [field: SerializeField] public List<DataSpriteBundle> DataSpriteBundles { get; private set; }
+    }
+}
