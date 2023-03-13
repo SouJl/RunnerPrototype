c6d8dfde841b42f616aa49f2cde0ae13ef037748
SouJl <meln.alex97@gmail.com>
2023-03-13 16:28:57 +0700

    Create main scripts and test scene

---
+++ Assets/Runner/_Root/Scripts/Tool/Bundles/AssetBundleViewBase.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Runner.Scripts.Tool.Bundles
+{
+    internal class AssetBundleViewBase : MonoBehaviour
+    {
+        private const string UrlAssetBundleSprites = "google-drive_url";
+        private const string UrlAssetBundleAudio = "google-drive_url";
+
+        [SerializeField] private DataSpriteBundle[] _dataSpriteBundles;
+        [SerializeField] private DataAudioBundle[] _dataAudioBundles;
+
+
+        private AssetBundle _spritesAssetBundle;
+        private AssetBundle _audioAssetBundle;
+
+
+        protected IEnumerator DownloadAndSetAssetBundles()
+        {
+            yield return GetSpritesAssetBundle();
+            yield return GetAudioAssetBundle();
+
+            if (_spritesAssetBundle != null)
+                SetSpriteAssets(_spritesAssetBundle);
+            else
+                Debug.LogError($"AssetBundle {nameof(_spritesAssetBundle)} failed to load");
+
+            if (_audioAssetBundle != null)
+                SetAudioAssets(_audioAssetBundle);
+            else
+                Debug.LogError($"AssetBundle {nameof(_audioAssetBundle)} failed to load");
+        }
+
+        private IEnumerator GetSpritesAssetBundle()
+        {
+            UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(UrlAssetBundleSprites);
+
+            yield return request.SendWebRequest();
+
+            while (!request.isDone)
+                yield return null;
+
+            StateRequest(request, out _spritesAssetBundle);
+        }
+
+        private IEnumerator GetAudioAssetBundle()
+        {
+            UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(UrlAssetBundleAudio);
+
+            yield return request.SendWebRequest();
+
+            while (!request.isDone)
+                yield return null;
+
+            StateRequest(request, out _audioAssetBundle);
+        }
+
+        private void StateRequest(UnityWebRequest request, out AssetBundle assetBundle)
+        {
+            if (request.error == null)
+            {
+                assetBundle = DownloadHandlerAssetBundle.GetContent(request);
+                Debug.Log("Complete");
+            }
+            else
+            {
+                assetBundle = null;
+                Debug.LogError(request.error);
+            }
+        }
+
+        private void SetSpriteAssets(AssetBundle assetBundle)
+        {
+            foreach (DataSpriteBundle data in _dataSpriteBundles)
+                data.Image.sprite = assetBundle.LoadAsset<Sprite>(data.NameAssetBundle);
+        }
+
+        private void SetAudioAssets(AssetBundle assetBundle)
+        {
+            foreach (DataAudioBundle data in _dataAudioBundles)
+            {
+                data.AudioSource.clip = assetBundle.LoadAsset<AudioClip>(data.NameAssetBundle);
+                data.AudioSource.Play();
+            }
+        }
+    }
+}
---
+++ Assets/Runner/_Root/Scripts/Tool/Bundles/DataAudioBundle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Runner.Scripts.Tool.Bundles
+{
+    [Serializable]
+    internal class DataAudioBundle
+    {
+        [field: SerializeField] public string NameAssetBundle { get; private set; }
+        [field: SerializeField] public AudioSource AudioSource { get; private set; }
+    }
+}
