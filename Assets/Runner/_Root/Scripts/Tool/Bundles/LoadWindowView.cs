dc5f90dec0a09abd9fab47476c6a6359baa3479c
Александр Мельников <meln.alex97@gmail.com>
2023-03-13 23:50:07 +0700

    Implement addresables background load assets

--- Assets/Runner/_Root/Scripts/Tool/Bundles/LoadWindowView.cs
+++ Assets/Runner/_Root/Scripts/Tool/Bundles/LoadWindowView.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using System.Collections.Generic;
+using System;
 
 namespace Runner.Scripts.Tool.Bundles
 {
@@ -12,31 +13,57 @@ namespace Runner.Scripts.Tool.Bundles
         [SerializeField] private Button _loadSpriteAssetsButton;
         [SerializeField] private Button _loadAudioAssetsButton;
         [SerializeField] private Button _changeBackgroundButton;
-
-        [Header("Addressables")]
+        
+        [Space(10)]
+        [Header("Addressables Spawn Buttons")]
         [SerializeField] private AssetReference _spawningButtonPrefab;
         [SerializeField] private RectTransform _spawnedButtonsContainer;
         [SerializeField] private Button _spawnAssetButton;
+        
+        [Space(10)]
+        [Header("Addressables Background")]
+        [SerializeField] private AssetReference _backgroundImage;
+        [SerializeField] private Image _backgroundImagePlacement;
+        [SerializeField] private Button _addBackgroundButton;
+        [SerializeField] private Button _removeBackgroundButton;
+
 
         private readonly List<AsyncOperationHandle<GameObject>> _addressablePrefabs =
             new List<AsyncOperationHandle<GameObject>>();
 
+        private AsyncOperationHandle<Sprite> _loadedBackgroundSprite;
+
         private void Start()
+        {
+            SubsribeButtons();
+        }
+
+        private void OnDestroy()
+        {
+            UnsubscribeButtons();
+            DespawnPrefabs();
+        }
+        private void SubsribeButtons()
         {
             _loadSpriteAssetsButton.onClick.AddListener(LoadSpriteAssets);
             _loadAudioAssetsButton.onClick.AddListener(LoadAudioAssets);
             _changeBackgroundButton.onClick.AddListener(LoadAndChangeBackground);
             _spawnAssetButton.onClick.AddListener(SpawnPrefab);
+
+            _addBackgroundButton.onClick.AddListener(AddBackground);
+            _removeBackgroundButton.onClick.AddListener(RemoveBackground);
         }
 
-        private void OnDestroy()
+
+        private void UnsubscribeButtons()
         {
             _loadSpriteAssetsButton.onClick.RemoveListener(LoadSpriteAssets);
             _loadAudioAssetsButton.onClick.RemoveListener(LoadAudioAssets);
             _changeBackgroundButton.onClick.RemoveListener(LoadAndChangeBackground);
             _spawnAssetButton.onClick.RemoveAllListeners();
 
-            DespawnPrefabs();
+            _addBackgroundButton.onClick.RemoveListener(AddBackground);
+            _removeBackgroundButton.onClick.RemoveListener(RemoveBackground);
         }
 
         private void LoadSpriteAssets()
@@ -72,5 +99,34 @@ namespace Runner.Scripts.Tool.Bundles
 
             _addressablePrefabs.Clear();
         }
+
+        private void AddBackground()
+        {
+            if (!_loadedBackgroundSprite.IsValid())
+            {
+                _loadedBackgroundSprite = Addressables.LoadAssetAsync<Sprite>(_backgroundImage);
+                _loadedBackgroundSprite.Completed += OnBackgroundLoaded;
+            }
+        }
+
+        private void RemoveBackground()
+        {
+            if (_loadedBackgroundSprite.IsValid())
+            {
+                _loadedBackgroundSprite.Completed -= OnBackgroundLoaded;
+                Addressables.Release(_loadedBackgroundSprite);
+                SetBackgroundSprite(null);
+            }
+        }
+
+        private void OnBackgroundLoaded(AsyncOperationHandle<Sprite> asyncOperationHandle)
+        {
+            asyncOperationHandle.Completed -= OnBackgroundLoaded;
+            SetBackgroundSprite(asyncOperationHandle.Result);
+        }
+
+        private void SetBackgroundSprite(Sprite sprite) =>
+            _backgroundImagePlacement.sprite = sprite;
     }
+
 }
