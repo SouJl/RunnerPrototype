c3838473ef16c4f42df6d07bb123046a41ddab43
Александр Мельников <meln.alex97@gmail.com>
2023-05-18 21:18:47 +0700

    Add Japan font and create dropdown localization

---
+++ Assets/Runner/_Root/Scripts/Tool/Localization/Text/DropDownTextLocalization.cs
@@ -0,0 +1,69 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+using UnityEngine.Localization.Tables;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Runner.Tool.Localization
+{
+    internal class DropDownTextLocalization : TextLocalization
+    {
+        [SerializeField] private TMP_Dropdown _dropDown;
+
+        [Header("DropDownText Settings")]
+        [SerializeField] private string _tableName;
+        [SerializeField] private string _selectedLocalizationTag;
+        [SerializeField] private string _optionsLocalizationTag;
+
+
+        private void OnValidate()
+        {
+            _dropDown ??= GetComponent<TMP_Dropdown>();
+        }
+
+        protected override void ChangeFont()
+        {
+            LocalizationSettings.AssetDatabase.GetTableAsync(tableFontName).Completed +=
+             handle =>
+             {
+                 if (handle.Status == AsyncOperationStatus.Succeeded)
+                 {
+                     AssetTable table = handle.Result;
+                    _dropDown.itemText.font = table.GetAssetAsync<TMP_FontAsset>(localizationFontTag).Result;
+                    _dropDown.captionText.font = table.GetAssetAsync<TMP_FontAsset>(localizationFontTag).Result;
+                 }
+                 else
+                 {
+                     string errorMessage = $"[{GetType().Name}] Could not load Asset Table: {handle.OperationException}";
+                     Debug.LogError(errorMessage);
+                 }
+             };
+        }
+
+        protected override void ChangeText()
+        {
+            LocalizationSettings.StringDatabase.GetTableAsync(_tableName).Completed +=
+              handle =>
+              {
+                  if (handle.Status == AsyncOperationStatus.Succeeded)
+                  {
+                      StringTable table = handle.Result;
+                      string[] dropValues = table.GetEntry(_optionsLocalizationTag)?.GetLocalizedString().Split(";");
+                      int i = 0;
+                      foreach(var option in _dropDown.options)
+                      {
+                          option.text = dropValues[i];
+                          i++;
+                      }
+
+                      _dropDown.captionText.text = table.GetEntry(_selectedLocalizationTag)?.GetLocalizedString();
+                  }
+                  else
+                  {
+                      string errorMessage = $"[{GetType().Name}] Could not load String Table: {handle.OperationException}";
+                      Debug.LogError(errorMessage);
+                  }
+              };
+        }
+    }
+}
---
+++ Assets/Runner/_Root/Scripts/Tool/Localization/Text/LabelLocalization.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.Localization.Tables;
+using UnityEngine.Localization.Settings;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using TMPro;
+
+namespace Runner.Tool.Localization
+{
+    internal class LabelLocalization : TextLocalization
+    {
+        [SerializeField] private TMP_Text _localizedText;
+
+        [Header("Label Settings")]
+        [SerializeField] private string _tableName;
+        [SerializeField] private string _localizationTag;
+
+        private void OnEnable()
+        {
+            _localizedText ??= GetComponent<TMP_Text>();
+        }
+
+        protected override void ChangeFont()
+        {
+            LocalizationSettings.AssetDatabase.GetTableAsync(tableFontName).Completed +=
+               handle =>
+               {
+                   if (handle.Status == AsyncOperationStatus.Succeeded)
+                   {
+                       AssetTable table = handle.Result;
+                       _localizedText.font = table.GetAssetAsync<TMP_FontAsset>(localizationFontTag).Result;
+                   }
+                   else
+                   {
+                       string errorMessage = $"[{GetType().Name}] Could not load Asset Table: {handle.OperationException}";
+                       Debug.LogError(errorMessage);
+                   }
+               };
+        }
+
+        protected override void ChangeText()
+        {
+            LocalizationSettings.StringDatabase.GetTableAsync(_tableName).Completed += 
+                handle => 
+                {
+                    if (handle.Status == AsyncOperationStatus.Succeeded)
+                    {
+                        StringTable table = handle.Result;
+                        _localizedText.text = table.GetEntry(_localizationTag)?.GetLocalizedString();
+                    }
+                    else
+                    {
+                        string errorMessage = $"[{GetType().Name}] Could not load String Table: {handle.OperationException}";
+                        Debug.LogError(errorMessage);
+                    }
+                };
+        }
+    }
+}
